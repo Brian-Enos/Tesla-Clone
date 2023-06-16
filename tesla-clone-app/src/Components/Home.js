1e6a4699d8fa13c131352db57bd92693bab15f1e
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 17:06:15 +0300

    rendering the file content to App.js

---
+++ tesla-clone-app/src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <div className="Container">
+      Home
+    </div>
+  )
+}
+
+export default Home
