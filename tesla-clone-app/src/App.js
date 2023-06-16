9fd50b1c971c89409a661f3fa8427d47da1f7133
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 17:07:22 +0300

    Pushing all the changes made in .json files and modules

--- tesla-clone-app/src/App.js
+++ tesla-clone-app/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from "./Components/Header";
+import Home from "./Components/Home"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <Home />
     </div>
   );
 }
