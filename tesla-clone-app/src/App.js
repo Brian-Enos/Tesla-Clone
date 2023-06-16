7a5586c7c731c2f42783a8324d3ac4a7ec3f2844
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 16:41:50 +0300

    rendered the header component

--- tesla-clone-app/src/App.js
+++ tesla-clone-app/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
