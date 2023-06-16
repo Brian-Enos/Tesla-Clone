188562021244f86e95d12ade4685654f6d5ad437
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 18:22:19 +0300

    Moved the button group and the arrow down classes into a new div container with the class name set to buttons

--- tesla-clone-app/src/Components/Section.js
+++ tesla-clone-app/src/Components/Section.js
@@ -10,24 +10,27 @@ function Section() {
         <p>Order Online for Touchless Delivery</p>
       </div>
 
-      <div className="ButtonGroup">
+      <div className="Buttons">
+        <div className="ButtonGroup">
 
-        <div className="LeftButton">
-          Custom Order
+          <div className="LeftButton">
+            Custom Order
+          </div>
+
+          <div className='RightButton'>
+            Existing Inventory
+          </div>
+
         </div>
 
-        <div className='RightButton'>
-          Existing Inventory
+        <div className="DownArrow">
+          <img src={DownArrow} />
         </div>
-
-
+        
       </div>
 
-      <div className="DownArrow">
-        <img src={DownArrow} />
-      </div>
-
-
+      
+      
 
     </div>
   )
