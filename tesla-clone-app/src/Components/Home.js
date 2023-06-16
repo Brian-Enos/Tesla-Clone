bb799de8807267203ec8f865df6a109f031b9ac8
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 17:20:22 +0300

    Styled the Wrap class

--- tesla-clone-app/src/Components/Home.js
+++ tesla-clone-app/src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react' ;
 import styled from "styled-components";
+import Section from "./Section";
 
 function Home() {
   return (
     <div className="Container">
-      Home
+     <Section />
     </div>
   )
 }
---
+++ tesla-clone-app/src/Components/Section.js
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+function Section() {
+  return (
+    <div className="Wrap">Section</div>
+  )
+}
+
+export default Section
