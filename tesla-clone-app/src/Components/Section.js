9079244a4182758e24c97a0f4a08239a1aaab80e
Brian-Enos <brianenos9199@gmail.com>
2023-06-16 17:32:01 +0300

    Added a new div with a classname of Item text and put the necessary content in it

--- tesla-clone-app/src/Components/Section.js
+++ tesla-clone-app/src/Components/Section.js
@@ -3,7 +3,12 @@ import React from 'react'
 
 function Section() {
   return (
-    <div className="Wrap">Section</div>
+    <div className="Wrap">
+      <div className="ItemText">
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </div>
+    </div>
   )
 }
 
