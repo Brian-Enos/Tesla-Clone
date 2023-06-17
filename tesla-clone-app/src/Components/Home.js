6640e2a477be98ebc351381b94913d1007b78a3c
Brian-Enos <brianenos9199@gmail.com>
2023-06-17 11:32:31 +0300

    declared a sectionStyle object to hold the properties of the image to be applied to the file

--- tesla-clone-app/src/Components/Home.js
+++ tesla-clone-app/src/Components/Home.js
@@ -1,31 +1,47 @@
-import React from 'react' ;
-import styled from "styled-components";
+import React from 'react';
 import Section from "./Section";
 
 import ModelS from "../../src/Images/model-s.jpg";
+import ModelY from "../../src/Images/model-y.jpg";
+import Model3 from "../../src/Images/model-3.jpg";
+import ModelX from "../../src/Images/model-x.jpg";
 
 function Home() {
   return (
     <div className="Container">
-     <Section
-      title="Model S"
-      description='Order Online for Touchless Delivery'
-      backgroundImage={ModelS}
-      leftBtnText="Custom Order"
-      rightBtnText="Existing Inventory"
+      <Section
+        title="Model S"
+        description='Order Online for Touchless Delivery'
+        backgroundImage={ModelS}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
       />
 
-     <Section 
-     />
+      <Section
+        title="Model Y"
+        description='Order Online for Touchless Delivery'
+        backgroundImage={ModelY}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
 
-     <Section />
+      <Section
+        title="Model 3"
+        description='Order Online for Touchless Delivery'
+        backgroundImage={Model3}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
 
-     <Section 
-     />
-
-
+      <Section
+        title="Model X"
+        description='Order Online for Touchless Delivery'
+        backgroundImage={ModelX}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
