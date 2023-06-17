44c04b5fcd5168f8a5826b28a0003ba118508758
Brian-Enos <brianenos9199@gmail.com>
2023-06-17 10:23:18 +0300

    Assigned props to section to be used for rendering

--- tesla-clone-app/src/Components/Home.js
+++ tesla-clone-app/src/Components/Home.js
@@ -2,12 +2,28 @@ import React from 'react' ;
 import styled from "styled-components";
 import Section from "./Section";
 
+import ModelS from "../../public/Images/model-s.jpg";
+
 function Home() {
   return (
     <div className="Container">
+     <Section
+      title="Model S"
+      description='Order Online'
+      backgroundImage={ModelS}
+      leftBtnText="Custom Order"
+      rightBtnText="Existing Inventory"
+      />
+
+     <Section 
+     />
+
      <Section />
-     <Section />
-     <Section />
+
+     <Section 
+     />
+
+
     </div>
   )
 }
