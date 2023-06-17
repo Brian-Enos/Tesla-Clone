2527f11fd6398df8ec54f3bff7354327b9a3041c
Brian-Enos <brianenos9199@gmail.com>
2023-06-17 12:37:17 +0300

    Added an anchor tag and a div for the menu inside the header container

--- tesla-clone-app/src/Components/Header.js
+++ tesla-clone-app/src/Components/Header.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import HeaderImg from "../Images/logo.svg";
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="HContainer">
+      <a>
+        <img src={HeaderImg} />
+      </a>
+
+      <div className="Menu">
+        <p><a href="#">Model S</a></p>
+        <p><a href="#">Model 3</a></p>
+        <p><a href="#">Model X</a></p>
+        <p><a href="#">Model Y</a></p>
+      </div>
+    </div>
   )
 }
 
